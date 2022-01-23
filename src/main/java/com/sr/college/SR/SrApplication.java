5fb5a1c4734a41a86acce3a39c1fde8d26f42563
rajeh <dharmateja481@gmail.com>
2022-01-24 00:01:46 +0530

    first commit

---
+++ src/main/java/com/sr/college/SR/SrApplication.java
@@ -0,0 +1,13 @@
+package com.sr.college.SR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrApplication.class, args);
+	}
+
+}
